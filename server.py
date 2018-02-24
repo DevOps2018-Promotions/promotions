4b7098fbc59e9b1742c67946991d3bdf29bb6a2b
Songnan Zhang <sz1982@nyu.edu>
2018-02-24 02:04:16 +0000

    change pet into promotion in server.py

--- server.py
+++ server.py
@@ -13,15 +13,15 @@
 # limitations under the License.
 
 """
-Pet Store Service
+Promotion Service
 
 Paths:
 ------
-GET /pets - Returns a list all of the Pets
-GET /pets/{id} - Returns the Pet with a given id number
-POST /pets - creates a new Pet record in the database
-PUT /pets/{id} - updates a Pet record in the database
-DELETE /pets/{id} - deletes a Pet record in the database
+GET /promotions - Returns a list all of the Promotions
+GET /promotions/{promotion_id} - Returns the Promotion with a given promotion_id number
+POST /promotions - creates a new Promotion record in the database
+PUT /promotions/{id} - updates a Promotion record in the database
+DELETE /promotions/{id} - deletes a Promotion record in the database
 """
 
 import os
@@ -46,9 +46,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'please, tell nobody... Shhhh'
 app.config['LOGGING_LEVEL'] = logging.INFO
 
-# Initialize SQLAlchemy
-#db = SQLAlchemy(app)
-
 # Pull options from environment
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 PORT = os.getenv('PORT', '5000')
@@ -56,10 +53,13 @@ PORT = os.getenv('PORT', '5000')
 ######################################################################
 # Error Handlers
 ######################################################################
+
+
 @app.errorhandler(DataValidationError)
 def request_validation_error(error):
     """ Handles Value Errors from bad data """
     return bad_request(error)
+
 
 @app.errorhandler(400)
 def bad_request(error):
@@ -68,12 +68,14 @@ def bad_request(error):
     app.logger.info(message)
     return jsonify(status=400, error='Bad Request', message=message), 400
 
+
 @app.errorhandler(404)
 def not_found(error):
     """ Handles resources not found with 404_NOT_FOUND """
     message = error.message or str(error)
     app.logger.info(message)
     return jsonify(status=404, error='Not Found', message=message), 404
+
 
 @app.errorhandler(405)
 def method_not_supported(error):
@@ -82,12 +84,14 @@ def method_not_supported(error):
     app.logger.info(message)
     return jsonify(status=405, error='Method not Allowed', message=message), 405
 
+
 @app.errorhandler(415)
 def mediatype_not_supported(error):
     """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
     message = error.message or str(error)
     app.logger.info(message)
     return jsonify(status=415, error='Unsupported media type', message=message), 415
+
 
 @app.errorhandler(500)
 def internal_server_error(error):
@@ -103,62 +107,70 @@ def internal_server_error(error):
 @app.route('/')
 def index():
     """ Root URL response """
-    return jsonify(name='Pet Demo REST API Service',
+    return jsonify(name='Promotion API Service',
                    version='1.0',
-                   paths=url_for('list_pets', _external=True)
-                  ), status.HTTP_200_OK
+                   paths=url_for('list_pets', _external=True)), status.HTTP_200_OK
 
 ######################################################################
-# LIST ALL PETS
+# LIST ALL PROMOTIONS
 ######################################################################
-@app.route('/pets', methods=['GET'])
-def list_pets():
-    """ Returns all of the Pets """
-    pets = []
-    category = request.args.get('category')
+
+
+@app.route('/promotions', methods=['GET'])
+def list_promotions():
+    """ Returns all of the Promotions """
+    promotions = []
+    promotion_id = request.args.get('promotion_id')
     name = request.args.get('name')
-    if category:
-        pets = Promotion.find_by_category(category)
+    product_id = request.args.get('product_id')
+    if promotion_id:
+        promotions = Promotion.find_by_category(promotion_id)
     elif name:
-        pets = Promotion.find_by_name(name)
+        promotions = Promotion.find_by_name(name)
+    elif product_id:
+        promotions = Promotion.find_by_name(product_id)
     else:
-        pets = Promotion.all()
+        promotions = Promotion.all()
 
-    results = [pet.serialize() for pet in pets]
+    results = [promotion.serialize() for promotion in promotions]
     return make_response(jsonify(results), status.HTTP_200_OK)
 
 
 ######################################################################
-# RETRIEVE A PET
+# RETRIEVE A PROMOTION
 ######################################################################
-@app.route('/pets/<int:pet_id>', methods=['GET'])
-def get_pets(pet_id):
-    """
-    Retrieve a single Pet
-
-    This endpoint will return a Pet based on it's id
-    """
-    pet = Promotion.find(pet_id)
-    if not pet:
-        raise NotFound("Pet with id '{}' was not found.".format(pet_id))
-    return make_response(jsonify(pet.serialize()), status.HTTP_200_OK)
 
 
-######################################################################
-# ADD A NEW PET
-######################################################################
-@app.route('/pets', methods=['POST'])
-def create_pets():
+@app.route('/promotions/<int:promotion_id>', methods=['GET'])
+def get_promotions(promotion_id):
     """
-    Creates a Pet
-    This endpoint will create a Pet based the data in the body that is posted
+    Retrieve a single Promotion
+
+    This endpoint will return a Promotion based on it's id
+    """
+    promotion = Promotion.find(promotion_id)
+    if not promotion:
+        raise NotFound("Promotion with id '{}' was not found.".format(promotion_id))
+    return make_response(jsonify(promotion.serialize()), status.HTTP_200_OK)
+
+
+######################################################################
+# ADD A NEW PROMOTION
+######################################################################
+
+
+@app.route('/promotions', methods=['POST'])
+def create_promotions():
+    """
+    Creates a Promotion
+    This endpoint will create a Promotion based the data in the body that is posted
     """
     check_content_type('application/json')
-    pet = Promotion()
-    pet.deserialize(request.get_json())
-    pet.save()
-    message = pet.serialize()
-    location_url = url_for('get_pets', pet_id=pet.promotion_id, _external=True)
+    promotion = Promotion()
+    promotion.deserialize(request.get_json())
+    promotion.save()
+    message = promotions.serialize()
+    location_url = url_for('get_promotions', promotion_id=promotion.promotion_id, _external=True)
     return make_response(jsonify(message), status.HTTP_201_CREATED,
                          {
                              'Location': location_url
@@ -166,48 +178,50 @@ def create_pets():
 
 
 ######################################################################
-# UPDATE AN EXISTING PET
+# UPDATE AN EXISTING PROMOTION
 ######################################################################
-@app.route('/pets/<int:pet_id>', methods=['PUT'])
-def update_pets(pet_id):
+@app.route('/promotions/<int:promotion_id>', methods=['PUT'])
+def update_promotions(promotion_id):
     """
-    Update a Pet
+    Update a Promotion
 
-    This endpoint will update a Pet based the body that is posted
+    This endpoint will update a Promotion based the body that is posted
     """
     check_content_type('application/json')
-    pet = Promotion.find(pet_id)
-    if not pet:
-        raise NotFound("Pet with id '{}' was not found.".format(pet_id))
-    pet.deserialize(request.get_json())
-    pet.id = pet_id
-    pet.save()
-    return make_response(jsonify(pet.serialize()), status.HTTP_200_OK)
+    promotion = Promotion.find(promotion_id)
+    if not promotion:
+        raise NotFound("Promotion with id '{}' was not found.".format(promotion_id))
+    promotion.deserialize(request.get_json())
+    promotion.id = promotion_id
+    promotion.save()
+    return make_response(jsonify(promotion.serialize()), status.HTTP_200_OK)
 
 
 ######################################################################
-# DELETE A PET
+# DELETE A PROMOTION
 ######################################################################
-@app.route('/pets/<int:pet_id>', methods=['DELETE'])
-def delete_pets(pet_id):
+@app.route('/promotions/<int:promotion_id>', methods=['DELETE'])
+def delete_promotions(promotion_id):
     """
-    Delete a Pet
+    Delete a Promotion
 
-    This endpoint will delete a Pet based the id specified in the path
+    This endpoint will delete a Promotion based the id specified in the path
     """
-    pet = Promotion.find(pet_id)
-    if pet:
-        pet.delete()
+    promotion = Promotion.find(promotion_id)
+    if promotion:
+        promotion.delete()
     return make_response('', status.HTTP_204_NO_CONTENT)
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
 
+
 def init_db():
     """ Initialies the SQLAlchemy app """
     global app
     Promotion.init_db(app)
+
 
 def check_content_type(content_type):
     """ Checks that the media type is correct """
@@ -215,6 +229,7 @@ def check_content_type(content_type):
         return
     app.logger.error('Invalid Content-Type: %s', request.headers['Content-Type'])
     abort(415, 'Content-Type must be {}'.format(content_type))
+
 
 def initialize_logging(log_level=logging.INFO):
     """ Initialized the default logging to STDOUT """
@@ -240,10 +255,11 @@ def initialize_logging(log_level=logging.INFO):
 ######################################################################
 #   M A I N
 ######################################################################
+
 if __name__ == "__main__":
-    print "========================================="
-    print " P E T   S E R V I C E   S T A R T I N G"
-    print "========================================="
+    print "===================================================="
+    print " P R O M O T I O N   S E R V I C E   S T A R T I N G"
+    print "===================================================="
     initialize_logging(logging.INFO)
     init_db()  # make our sqlalchemy tables
     app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
