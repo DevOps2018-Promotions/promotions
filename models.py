57afb36c3e498fdfdd804a434e0fc276e5fc8941
Songnan Zhang <sz1982@nyu.edu>
2018-03-01 06:00:51 +0000

    make sure a bad update request will be denied as whole

--- models.py
+++ models.py
@@ -90,9 +90,12 @@ class Promotion(db.Model):
         if not isinstance(data, dict):
             raise DataValidationError('Invalid pet: body of request contained bad or no data')
         try:
-            self.name = "" + data['name']
-            self.product_id = 0 + data['product_id']
-            self.discount_ratio = 0.0 + data['discount_ratio']
+            new_name = "" + data['name']
+            new_product_id = 0 + data['product_id']
+            new_discount_ratio = 0.0 + data['discount_ratio']
+            self.name = new_name
+            self.product_id = new_product_id
+            self.discount_ratio = new_discount_ratio
             # self.start_date = data['start_date']
             # self.end_date = data['end_date']
         except KeyError as error:
